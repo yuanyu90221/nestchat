dd58ce12587da3780a492f832c8af80c7ab98771
yuanyu90221 <yuanyu90221@gmail.com>
2021-07-27 16:04:16 +0800

    change error code from number to string

--- src/exceptions/WsUnauth.exception.ts
+++ src/exceptions/WsUnauth.exception.ts
@@ -2,7 +2,7 @@ import { WsException } from "@nestjs/websockets";
 
 export class WsUnauthException extends WsException {
   public data: Object;
-  constructor(errCode: number, message: string) {
+  constructor(errCode: string, message: string) {
     super({errCode, message});
     this.message = message;
     this.data = this.getError();
